dd9c30e2a837ef436382fd5d76034278010f0cde
Hyacinthia M. Groeizaam <cinnymaria@gmail.com>
2021-05-19 11:09:52 +0200

    push the basics

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
-
+import Searchbar from './Searchbar';
 
 function App() {
   return (
     <div className="App">
-      Hello World!!
+      <Searchbar />
     </div>
   );
 }
